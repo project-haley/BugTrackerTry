7e3f523c1231b58dd986e7fa0ddfbe78511134de
project-haley <60005425+project-haley@users.noreply.github.com>
2022-02-13 07:30:52 -0600

    search attempt

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,34 @@
+﻿using BugTrackerTry.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTrackerTry.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SearchController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var projects = from m in _context.Projects
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                projects = projects.Where(s => s.Name!.Contains(searchString));
+            }
+
+            return View(await projects.ToListAsync());
+
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,20 @@
-﻿const searchButton = document.getElementById('search-button');
-const searchInput = document.getElementById('search-input');
-searchButton.addEventListener('click', () => {
-    const inputValue = searchInput.value;
-    alert(inputValue);
+﻿const searchButton = document.getElementById('searchButton');
+const searchInput = document.getElementById('searchInput');
+
+
+
+function Search() {
+    $.ajax({
+        url: '/Home/Index',
+        type: "GET",
+        dataType: "json",
+/*        data: { searchString: input },*/
+        contentType: "application/json; charset=utf-8"
+    });
+};
+
+searchButton.addEventListener('click', Search());
+
+$(document).ready(function () {
+    $('searchButton').click(Search());
 });
